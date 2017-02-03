56bf7c79e6558677bfe57ed9a7b1bd2bea35fb06
JanuitaM <januita.marpaung@moonlay.com>
2017-02-03 11:58:54 +0700

    add field name in unit

--- src/master/unit-validator.js
+++ src/master/unit-validator.js
@@ -1,4 +1,4 @@
-require("should"); 
+require("should");
 
 module.exports = function (data) {
     data.should.not.equal(null);
@@ -7,9 +7,12 @@ module.exports = function (data) {
     data.should.have.property('code');
     data.code.should.be.String();
 
+    data.should.have.property('name');
+    data.code.should.be.String();
+
     data.should.have.property('divisionId');
     data.divisionId.should.be.Object();
-    
+
     data.should.have.property('division');
     data.division.should.be.Object();
 
@@ -20,6 +23,6 @@ module.exports = function (data) {
     data.store.should.be.Object();
 
     data.should.have.property('description');
-    data.description.should.be.String(); 
-    
+    data.description.should.be.String();
+
 };
