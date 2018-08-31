8e9d1c20099e0523669afb2f4c67ffebedfc37db
AchmadSetiaji <achmad.setiaji@moonlay.com>
2018-08-31 15:15:41 +0700

    purchase correction models

--- src/validator.js
+++ src/validator.js
@@ -118,6 +118,7 @@ module.exports = {
         unitPaymentCorrectionNote: require("./purchasing/unit-payment-correction-note-validator"),
         unitPaymentCorrectionNoteItem: require("./purchasing/unit-payment-correction-note-item-validator"),
         unitReceiptNote : require('./purchasing/unit-receipt-note-validator'),
-        unitPaymentOrder : require('./purchasing/unit-payment-order-validator')
+        unitPaymentOrder : require('./purchasing/unit-payment-order-validator'),
+        purchaseCorrectionValidator: require('./purchasing/purchase-correction-validator')
     }
 };
